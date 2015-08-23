e4a389f5118bfc501af78e717c29b530243192c4
Vamsee Chamakura <vamseems@gmail.com>
2015-08-24 01:56:57 +0530

    Restructure of the Repo, and skeleton android app

---
+++ Android/Genie/app/src/main/java/com/vchamakura/genie/LoginRegister.java
@@ -0,0 +1,20 @@
+package com.vchamakura.genie;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by vchamakura on 24/08/15 at 1:06 AM.
+ Copyrights reserved to Vamsee Chamakura
+ */
+public class LoginRegister extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+    }
+
+}
