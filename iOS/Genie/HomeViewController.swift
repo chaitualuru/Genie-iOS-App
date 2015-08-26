b4d3eedcbde9e5129830eb43b9278e7cb7f6c7a0
kcaluru <krishna_aluru@brown.edu>
2015-08-25 21:55:38 -0400

    added JSQMessages framework

--- iOS/Genie/HomeViewController.swift
+++ iOS/Genie/HomeViewController.swift
@@ -13,7 +13,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,9 +23,6 @@ class HomeViewController: UIViewController {
     
 
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
