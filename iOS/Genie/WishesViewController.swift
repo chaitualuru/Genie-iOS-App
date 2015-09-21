fa15b3e860f7aba9a0902ee4da6f10803d3af949
Vamsee Chamakura <vamseems@gmail.com>
2015-09-22 00:36:10 +0530

    Sideways swipe

---
+++ iOS/Genie/WishesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WishesViewController.swift
+//  Genie
+//
+//  Created by Vamsee Chamakura on 21/09/15.
+//  Copyright © 2015 genie. All rights reserved.
+//
+
+import UIKit
+
+class WishesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
