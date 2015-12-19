a100490427df307f01d9712cf7b333a7fffc6e00
Vamsee Chamakura <vamseems@gmail.com>
2015-12-19 19:28:59 +0530

    Profile View done

--- iOS/Genie/ProfileViewController.swift
+++ iOS/Genie/ProfileViewController.swift
@@ -13,12 +13,13 @@ class ProfileViewController: UIViewController {
 
     @IBOutlet weak var whiteViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var profileImg: UIImageView!
-//    @IBOutlet weak var userName: UITextField!
-//    
-//    @IBOutlet weak var userLocation: UILabel!
-//    @IBOutlet weak var userMobile: UITextField!
-//    @IBOutlet weak var userEmail: UITextField!
-//    
+//
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var fullName: UILabel!
+
+    @IBOutlet weak var userMobile: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
+//
 
     @IBOutlet weak var profileImgTop: NSLayoutConstraint!
     
@@ -35,55 +36,36 @@ class ProfileViewController: UIViewController {
         
         self.user = self.ref.authData
         
-//        let firstNameRef = ref.childByAppendingPath("users/" + (self.user?.uid)!)
-//        firstNameRef.observeEventType(.Value, withBlock: {
-//            snapshot in
-//            self.senderDisplayName = (snapshot.value.objectForKey("first_name") as! String) + " " + (snapshot.value.objectForKey("last_name") as! String)
-//            self.mobileNumber = snapshot.value.objectForKey("mobile_number") as! String
-//            self.emailAddress = snapshot.value.objectForKey("email_address") as! String
-//            self.userName.text = self.senderDisplayName
-//            self.userMobile.text = "   " + self.mobileNumber
-//            self.userEmail.text = "   " + self.emailAddress
-//            }, withCancelBlock: { error in
-//                print(error.description)
-//        })
+        let firstNameRef = ref.childByAppendingPath("users/" + (self.user?.uid)!)
+        firstNameRef.observeEventType(.Value, withBlock: {
+            snapshot in
+            self.senderDisplayName = (snapshot.value.objectForKey("first_name") as! String) + " " + (snapshot.value.objectForKey("last_name") as! String)
+            self.mobileNumber = snapshot.value.objectForKey("mobile_number") as! String
+            self.emailAddress = snapshot.value.objectForKey("email_address") as! String
+            self.fullName.text = self.senderDisplayName
+            self.username.text = snapshot.value.objectForKey("username") as? String
+            self.userMobile.text = self.mobileNumber
+            self.userEmail.text = self.emailAddress
+            }, withCancelBlock: { error in
+                print(error.description)
+        })
 
-//        // Dismiss keyboard on tap --------------------------------------------------------------
-//        
-//        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-//        view.addGestureRecognizer(tap)
-//        // --------------------------------------------------------------------------------------
-//        
+        // Dismiss keyboard on tap --------------------------------------------------------------
         
-          profileImgTop.constant = 0.3 * 0.3 * UIScreen.mainScreen().bounds.height
-//
-//        self.profileImg.layer.cornerRadius = self.profileImg.frame.size.width / 2;
-//        self.profileImg.layer.borderWidth = 3.0
-//        self.profileImg.layer.borderColor = UIColor.whiteColor().CGColor
-//        self.profileImg.clipsToBounds = true;
-//        
-//        let purple = UIColor(red: 79/255, green: 67/255, blue: 135/255, alpha: 1)
-//        
-//        userMobile.layer.addBorder(UIRectEdge.Bottom, color: purple, thickness: 0.5)
-//        userEmail.layer.addBorder(UIRectEdge.Bottom, color: purple, thickness: 0.5)
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+            
+        self.profileImgTop.constant = 0.3 * 0.3 * UIScreen.mainScreen().bounds.height
+       
+        let lineColor = UIColor.lightGrayColor()
+        
+        userMobile.layer.addBorder(UIRectEdge.Bottom, color: lineColor, thickness: 0.5)
+        userEmail.layer.addBorder(UIRectEdge.Bottom, color: lineColor, thickness: 0.5)
         
     }
     
     @IBAction func closeProfileVC(sender: UIButton) {
-//        let new_number = self.userMobile.text
-//        let new_email = self.userEmail.text
-//        
-//        let full_name = self.userName.text!.componentsSeparatedByString(" ")
-//        
-//        let userRef = ref.childByAppendingPath("users/" + (self.user?.uid)!)
-//
-//        userRef.updateChildValues([
-//            "email_address": new_email!.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()),
-//            "mobile_number": new_number!.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()),
-//            "first_name": full_name[0],
-//            "last_name": full_name[1]
-//            ])
-//        self.dismissViewControllerAnimated(true, completion: nil)
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     func dismissKeyboard() {
@@ -94,18 +76,6 @@ class ProfileViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
 }
 
 extension CALayer {
