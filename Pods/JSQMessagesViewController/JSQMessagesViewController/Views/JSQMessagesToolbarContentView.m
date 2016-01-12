f0c59976a35dc7c5e38ce8cff4f666e384f51857
Vamsee Chamakura <vamseems@gmail.com>
2016-01-13 01:41:26 +0530

    EZ Access icons changed

--- Pods/JSQMessagesViewController/JSQMessagesViewController/Views/JSQMessagesToolbarContentView.m
+++ Pods/JSQMessagesViewController/JSQMessagesViewController/Views/JSQMessagesToolbarContentView.m
@@ -45,9 +45,7 @@ const CGFloat kJSQMessagesToolbarContentViewHorizontalSpacingDefault = 8.0f;
 - (IBAction)customKeyboardShow:(UIButton *)sender;
 - (IBAction)normalKeyboardShow:(UIButton *)sender;
 
-
 @end
-
 
 
 @implementation JSQMessagesToolbarContentView
@@ -72,19 +70,40 @@ const CGFloat kJSQMessagesToolbarContentViewHorizontalSpacingDefault = 8.0f;
     self.leftHorizontalSpacingConstraint.constant = kJSQMessagesToolbarContentViewHorizontalSpacingDefault;
     self.rightHorizontalSpacingConstraint.constant = kJSQMessagesToolbarContentViewHorizontalSpacingDefault;
     
-    UIImage *finance = [UIImage imageNamed:@"finance.png"];
-    UIImage *booking = [UIImage imageNamed:@"booking.png"];
-    UIImage *health = [UIImage imageNamed:@"health.png"];
-    UIImage *home = [UIImage imageNamed:@"home.png"];
-    UIImage *shopping = [UIImage imageNamed:@"shopping.png"];
-    UIImage *food = [UIImage imageNamed:@"food.png"];
+    UIImage *finance_before = [UIImage imageNamed:@"finance_before.png"];
+    UIImage *booking_before = [UIImage imageNamed:@"booking_before.png"];
+    UIImage *health_before = [UIImage imageNamed:@"health_before.png"];
+    UIImage *home_before = [UIImage imageNamed:@"home_before.png"];
+    UIImage *shopping_before = [UIImage imageNamed:@"shopping_before.png"];
+    UIImage *food_before = [UIImage imageNamed:@"food_before.png"];
+    UIImage *keyboard_before = [UIImage imageNamed:@"keyboard_before.png"];
+    
+    UIImage *finance_after = [UIImage imageNamed:@"finance_after.png"];
+    UIImage *booking_after = [UIImage imageNamed:@"booking_after.png"];
+    UIImage *health_after = [UIImage imageNamed:@"health_after.png"];
+    UIImage *home_after = [UIImage imageNamed:@"home_after.png"];
+    UIImage *shopping_after = [UIImage imageNamed:@"shopping_after.png"];
+    UIImage *food_after = [UIImage imageNamed:@"food_after.png"];
+    UIImage *keyboard_after = [UIImage imageNamed:@"keyboard_after.png"];
 
-    [self.financeButton setImage:finance forState:UIControlStateNormal];
-    [self.bookingButton setImage:booking forState:UIControlStateNormal];
-    [self.healthButton setImage:health forState:UIControlStateNormal];
-    [self.homeButton setImage:home forState:UIControlStateNormal];
-    [self.shoppingButton setImage:shopping forState:UIControlStateNormal];
-    [self.foodButton setImage:food forState:UIControlStateNormal];
+    [self.financeButton setImage:finance_before forState:UIControlStateNormal];
+    [self.bookingButton setImage:booking_before forState:UIControlStateNormal];
+    [self.healthButton setImage:health_before forState:UIControlStateNormal];
+    [self.homeButton setImage:home_before forState:UIControlStateNormal];
+    [self.shoppingButton setImage:shopping_before forState:UIControlStateNormal];
+    [self.foodButton setImage:food_before forState:UIControlStateNormal];
+    [self.keyboardButton setImage:keyboard_before forState:UIControlStateNormal];
+    
+    
+    [self.financeButton setImage:finance_after forState:(UIControlStateSelected)];
+    [self.bookingButton setImage:booking_after forState:(UIControlStateSelected)];
+    [self.healthButton setImage:health_after forState:(UIControlStateSelected)];
+    [self.homeButton setImage:home_after forState:(UIControlStateSelected)];
+    [self.shoppingButton setImage:shopping_after forState:(UIControlStateSelected)];
+    [self.foodButton setImage:food_after forState:(UIControlStateSelected)];
+    [self.keyboardButton setImage:keyboard_after forState:(UIControlStateSelected)];
+    
+    self.keyboardButton.selected = YES;
     
     
     self.backgroundColor = [UIColor clearColor];
@@ -206,16 +225,34 @@ const CGFloat kJSQMessagesToolbarContentViewHorizontalSpacingDefault = 8.0f;
     UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 160)];
     view.backgroundColor = [UIColor grayColor];
     
-//    [self.textView resignFirstResponder];
+    [self.textView resignFirstResponder];
     self.textView.inputView = view;
-//    [self.textView becomeFirstResponder];
+    [self.textView becomeFirstResponder];
     [self.textView reloadInputViews];
+    NSArray* buttons = [NSArray arrayWithObjects:self.financeButton, self.bookingButton, self.healthButton, self.homeButton, self.foodButton, self.shoppingButton, self.keyboardButton, nil];
+    for (UIButton* button in buttons) {
+        if (button == sender) {
+            button.selected = YES;
+        }
+        else {
+            button.selected = NO;
+        }
+    }
 }
 
 - (IBAction)normalKeyboardShow:(UIButton *)sender {
-//    [self.textView resignFirstResponder];
+    [self.textView resignFirstResponder];
     self.textView.inputView = nil;
-//    [self.textView becomeFirstResponder];
+    [self.textView becomeFirstResponder];
     [self.textView reloadInputViews];
+    NSArray* buttons = [NSArray arrayWithObjects:self.financeButton, self.bookingButton, self.healthButton, self.homeButton, self.foodButton, self.shoppingButton, self.keyboardButton, nil];
+    for (UIButton* button in buttons) {
+        if (button == sender) {
+            button.selected = YES;
+        }
+        else {
+            button.selected = NO;
+        }
+    }
 }
 @end
